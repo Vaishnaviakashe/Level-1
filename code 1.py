5d1a12a52024a32f4dac3f2fbef9bcd2b0ffd7cf
Vaishnaviakashe <vaishnu0705@gmail.com>
2026-01-05 16:15:34 +0530

    updated

--- code 1.py
+++ code 1.py
@@ -1,1 +1,11 @@
-print("hello world")
+#Print number is positive ,negative or zero
+
+n = int(input())
+
+if n > 0:
+    print(n,":Positive")
+elif n < 0:
+    print(n,":Negative")
+else:
+    print(n,":Zero")
+    

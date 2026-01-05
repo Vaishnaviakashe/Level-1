569e4882cad5974c10352b56f544e4e2a6a42fc9
Vaishnaviakashe <vaishnu0705@gmail.com>
2026-01-05 16:20:23 +0530

    new file added

---
+++ Code 2.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    if year % 400 == 0:
+        print("Leap")
+        
+    elif year % 100 == 0:
+        print("Not Leap")
+    elif year % 4 == 0:
+        print("Leap")
+        
+    else:
+        print("Not Leap")
+        
+year = int(input("Enter a year:"))
+is_leap(year)

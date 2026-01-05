dd9ac8cbf233242dd0c8b31a101946b4832cbf70
Vaishnaviakashe <vaishnu0705@gmail.com>
2026-01-05 16:26:07 +0530

    New code added

---
+++ Code 1.py
@@ -0,0 +1,11 @@
+#Print number is positive ,negative or zero
+
+n = int(input("Enter a number:"))
+
+if n > 0:
+    print(n,":Positive")
+elif n < 0:
+    print(n,":Negative")
+else:
+    print(n,":Zero")
+    

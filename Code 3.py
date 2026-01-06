cf0e5d722eb949246e8d3aab305c7a8f9d06f09d
Vaishnaviakashe <vaishnu0705@gmail.com>
2026-01-06 23:27:58 +0530

    New code added

---
+++ Code 3.py
@@ -0,0 +1,9 @@
+#Print larger number wihtout max 
+
+a = int(input("Enter first a number:"))
+b = int(input("Enter second a number:"))
+
+if a > b :
+    print("Greater number:",a)
+else:
+    print("Greater number:",b)

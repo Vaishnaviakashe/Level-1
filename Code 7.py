75a53b97fba5f37826b20890d57692299dfc6b09
Vaishnaviakashe <akashevaishnavi07@gmail.com>
2026-01-16 14:47:22 +0530

    Code 7.py

--- Code 7.py
+++ Code 7.py
@@ -6,8 +6,8 @@ age = int(input("Enter age: "))
 if age < 0 or age > 120:
     print("Invalid !")
     
-elif age > 0 and age < 18:
+elif  age < 18:
     print("Child")
     
-elif age >= 18 :
+else :
     print("Adult")
